5dbc9b42e69476178229e5316c7b8c0c8cf84d82
Matteo Ronchetti <mttronchetti@gmail.com>
2017-10-26 19:24:27 +0200

    remote page

--- tests/test.py
+++ tests/test.py
@@ -7,11 +7,19 @@ CURRENT_PATH = os.path.abspath(os.path.dirname(__file__))
 
 
 class Test(unittest.TestCase):
-    def test_page_loading(self):
-        page = Raschietto.from_file(os.path.join(CURRENT_PATH, "pages", "1.html"))
-
+    def _test_page_1(self, page):
         self.assertEqual(Matcher("a#link")(page), "link text")
         self.assertEqual(Matcher.link("a#link")(page), "http://url.com")
         self.assertEqual(Matcher.image("img")(page), "http://url.com/image.jpg")
         self.assertEqual(Matcher("div.text")(page), "This is a sample of bold text")
+
+    def test_local_page_loading(self):
+        page = Raschietto.from_file(os.path.join(CURRENT_PATH, "..", "pages", "1.html"))
+        self._test_page_1(page)
+
+    def test_remote_page_loading(self):
+        page = Raschietto.from_url("http://matteo-ronchetti.github.io/raschietto/pages/1.html")
+        self._test_page_1(page)
+
+
 
